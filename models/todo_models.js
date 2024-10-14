4729d8bdb5a1e7b1c508197529c86e30bde8107d
Felicity Semetey <felicitysemetey628@gmail.com>
2024-10-14 23:24:28 +0000

    toJSON

--- models/todo_models.js
+++ models/todo_models.js
@@ -1,10 +1,14 @@
 import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
 
 const todoSchema = new Schema({
     title: {type: String, required: true},
     icon: {type: String, required: true},
     completed: {type: Boolean, default: false}
 });
+
+todoSchema.plugin(toJSON);
+
 
 // model function from mongoose. requires a name and a schema
 // we're creating a class called TodoModel that uses mongoose's function parameters(name, schema)
